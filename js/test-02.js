92266c5348fa86b0709b5ef8186e04b2a23b50b7
Vitaliy Maksymchuk <vitalii.v.maksymchuk@gmail.com>
2024-09-24 21:19:47 +0300

    1

---
+++ js/test-02.js
@@ -0,0 +1,81 @@
+'use strict';
+
+
+// ===01===
+
+// function checkAge(age) {
+
+//   if (age >= 18) {
+//     return `You are an adult`;
+//   };
+
+// };
+
+// console.log(checkAge(20));
+// console.log(checkAge(18));
+// console.log(checkAge(2));
+
+// ===end of 01===
+
+
+
+// ===02===
+
+// function checkStorage(available, ordered) {
+
+//     if (ordered > available) {
+
+//       return `Not enough goods in stock!`;
+
+//         } else {
+//            return `Order is processed, our manager will contact you`;
+//         };
+
+// };
+
+// console.log(checkStorage(20, 31));
+// console.log(checkStorage(20, 8));
+
+// ===end of 02===
+
+
+
+// ===03===
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ===end of 03===
+
+
+
+// ===??===
+
+// Функція checkPassword(password) порівнює переданий їй пароль (параметр password) зі збереженим паролем адміністратора (correctPassword) і повертає рядок з повідомленням про результат.
+
+// Використовуючи тернарний оператор, доповни функцію таким чином, щоб:
+
+// Якщо значення password і correctPassword збігаються, функція повертала рядок "Access granted"
+// В іншому випадку, функція повертала рядок "Access denied, wrong password!"
+
+function checkPassword(password) {
+    const correctPassword = "jqueryismyjam" = password ? `Access granted` : `Access denied, wrong password!`; 
+  }
+
+  console.log(checkPassword);
+  
---
+++ js/test-10.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+// ===01===
+
+// function checkAge(age) {
+
+//   if (age >= 18) {
+//     return `You are an adult`;
+//   };
+
+// };
+
+// console.log(checkAge(20));
+// console.log(checkAge(18));
+// console.log(checkAge(2));
+
+// ===end of 01===
+
+
+
+// ===02===
+
+// function checkStorage(available, ordered) {
+
+//     if (ordered > available) {
+
+//       return `Not enough goods in stock!`;
+
+//         } else {
+//            return `Order is processed, our manager will contact you`;
+//         };
+
+// };
+
+// console.log(checkStorage(20, 31));
+// console.log(checkStorage(20, 8));
+
+// ===end of 02===
+
+
+
+// ===03===
+
+
